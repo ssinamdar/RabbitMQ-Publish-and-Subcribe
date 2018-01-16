83104796ad42cee9577357694c6a4b4eb936ebe1
ssinamdar <sudhir.inamdar@gmail.com>
2018-01-16 11:43:17 +0530

    Add files via upload

---
+++ CreateGdmsNotificationCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RabbitMqServiceBus;
+
+namespace CMG.Wholesale.Api.Controllers.Appraisal
+{
+    [CommandMetaData("CMGGdmsNotificationEndPoint")]
+    public class CreateGdmsNotificationCommand : BaseCommand
+    {
+        public CreateGdmsNotificationCommand(Guid applicationId, Guid commandId, Guid busId)
+            : base(applicationId, commandId, busId)
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"ApplicationId:{ApplicationId} commandId:{CommandId} busId:{BusId}";
+        }
+    }
+}
---
+++ GdmsNotifificationController.cs
@@ -0,0 +1,74 @@
+﻿using AutoMapper;
+using CMG.Domain.Appraisal.Contracts;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using RabbitMqServiceBus;
+
+namespace CMG.Wholesale.Api.Controllers.Appraisal
+{
+    [RoutePrefix("api/GdmsNotifification")]
+    public class GdmsNotifificationController : ApiController
+    {
+        private readonly IGdmsNotificationManager _gdmsNotificationManager;
+        private readonly IMapper _mapper;
+        public GdmsNotifificationController(IGdmsNotificationManager gdmsNotificationManager, IMapper mapper)
+        {
+            if (gdmsNotificationManager == null) throw new ArgumentNullException(nameof(gdmsNotificationManager));
+            _gdmsNotificationManager = gdmsNotificationManager;
+
+            if (mapper == null) throw new ArgumentNullException(nameof(mapper));
+            _mapper = mapper;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [ExcludeFromCodeCoverage]
+        public async Task<IHttpActionResult> GetUpdatedStatus(HttpRequestMessage httpRequestMessage)
+        {
+            //Publish RabbitMq queue method()
+
+            await AddPushRequestToQueue("");
+            return Ok(HttpStatusCode.OK);
+
+            //Web.Config changes
+            /*
+
+             **********Install RabbitMQ and Erlang on the Machine before start coding.**********
+             * 
+            < section name = "RabbitMqBusConfigurationSection" type = "RabbitMqServiceBus.Configuration.RabbitMqBusConfigurationSection,RabbitMqServiceBus" />
+
+
+            < RabbitMqBusConfigurationSection >
+            < rabbitMqConnection uri = "" vhost = "CMGWholesale" port = "5672" connectionTimeout = "20" userName = "admin" password = "admin" hostname = "blrcswcmgv01" />
+            < deadletterexchange exchange = "cmg.exchange.dead" type = "direct" />
+            < RabbitMqBusEndPoints >
+            < add name = "CMGGdmsNotificationEndPoint" exchange = "cmg.exchange.wholesale" exchangetype = "direct" queue = "cmg-wholesale-CMGGdmsNotification-queue" TTL = "60" durable = "True" routingkey = "GdmsQueue" dead - letter - routing = "True" ></ add >
+            < add name = "CMGGdmsNotification-dead-EndPoint" exchange = "cmg.exchange.dead" exchangetype = "direct" queue = "cmg-wholesale-CMGGdmsNotification-dead-queue" TTL = "-1" durable = "True" routingkey = "GdmsQueueDead" dead - letter - routing = "False" ></ add >
+            </ RabbitMqBusEndPoints >
+            </ RabbitMqBusConfigurationSection > 
+
+                        Dll required to download from the Nuget package in Publish project folder:
+                                    RabbitMQ.Client and RabbitMqServiceBus dll (download nuget)
+
+
+            In Subscribe project install: 
+                    Autofac, log4net, RabbitMQ.Client, RabbitMqServerBus, Topshelf and Topshelf.Autofac
+                        */
+        }
+
+        private async static void AddPushRequestToQueue(string message)
+        {
+            Guid applicationId = Guid.NewGuid(); //one for an application
+            Guid busId = Guid.NewGuid(); //Each instance of publishing bus
+            var rmBus = new RabbitMqBus();
+            await Task.Run(() => rmBus.SendCommand(new CreateGdmsNotificationCommand(applicationId, Guid.NewGuid(), busId)
+            {
+                Message = message
+            }));
+        }
+    }
+}
